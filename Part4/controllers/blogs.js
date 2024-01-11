813355c76e36a148ccf18df56c9f8ae37d137b1e
olrut <121128961+olrut@users.noreply.github.com>
2024-01-11 12:18:04 +0200

    Part4 Step1

---
+++ Part4/controllers/blogs.js
@@ -0,0 +1,58 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+    Blog.find({}).then(blogs => {
+        response.json(blogs)
+    })
+})
+
+// blogsRouter.get('/:id', (request, response, next) => {
+//     Blog.findById(request.params.id)
+//         .then(note => {
+//             if (note) {
+//                 response.json(note)
+//             } else {
+//                 response.status(404).end()
+//             }
+//         })
+//         .catch(error => next(error))
+// })
+
+blogsRouter.post('/', (request, response, next) => {
+    const body = request.body
+
+    const blog = new Blog(body);
+
+    blog
+        .save()
+        .then(savedBlog => {
+            response.json(savedBlog)
+        })
+        .catch(error => next(error))
+})
+
+// blogsRouter.delete('/:id', (request, response, next) => {
+//     Blog.findByIdAndDelete(request.params.id)
+//         .then(() => {
+//             response.status(204).end()
+//         })
+//         .catch(error => next(error))
+// })
+
+// blogsRouter.put('/:id', (request, response, next) => {
+//     const body = request.body
+//
+//     const blog = {
+//         content: body.content,
+//         important: body.important,
+//     }
+//
+//     Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+//         .then(updatedNote => {
+//             response.json(updatedNote)
+//         })
+//         .catch(error => next(error))
+// })
+
+module.exports = blogsRouter
